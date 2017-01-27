aefa1f98336b2a62a8343972a2129b9f99560d97
Hayden Elza <hayden.elza@gmail.com>
2017-01-27 15:21:31 -0600

    Added some explaintions on variables

--- network_files/main_config.py
+++ network_files/main_config.py
@@ -1,5 +1,5 @@
-nosp_name = "Example_Name"
-full_name = "Example Name"
+nosp_name = "Example_Name"  # This will be used to create file names
+full_name = "Example Name"  # This will be used in email messages
 
 to_backup = []  # List of paths to directories or files to backup
 temp_directory   = ""  # Full path to temporary directory including the trailing slash; Can be None to archive directly to output directory
